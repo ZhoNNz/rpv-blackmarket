5b2914aeee0ab1d65a1b48fe18eceafa370e1fea
ZhoNNz <52080900+ZhoNNz@users.noreply.github.com>
2020-08-21 20:04:17 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,27 @@
-
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterCommand('blackmarket', function(source, rawCommand)
+	TriggerClientEvent("rpv-blackmarket:Erisim",source)
+    Wait(200)
+    TriggerClientEvent('chat:addMessage', -1, {
+        template = '<div class="chat-message durum"><b>BLACKMARKET STATUS :</b> Komut devreye girdi</div>',
+        args = { user, msg }
+    })
+end)
+
+RegisterCommand('hedef', function(source, rawCommand)
+
+	TriggerClientEvent("rpv-blackmarket:hedef",source)
+    Wait(200)
+    TriggerClientEvent('chat:addMessage', -1, {
+        template = '<div class="chat-message durum"><b>HEDEF YOLDA :</b> Birazdan gelecek</div>',
+        args = { user, msg }
+    })
+
+end)
+
 
 
 RegisterServerEvent("rpv-blackmarket:BuyItem")
@@ -11,11 +31,11 @@ AddEventHandler("rpv-blackmarket:BuyItem", function(amountToBuy,totalBuyPrice,it
 	if xPlayer.getMoney() >= totalBuyPrice then
 		xPlayer.removeMoney(totalBuyPrice)
 		xPlayer.addInventoryItem(itemName, amountToBuy)
-        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = itemLabel .. amountToBuy .. 'x' ..totalBuyPrice.. '$ You Paid'})
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = itemLabel .. amountToBuy .. 'x' ..totalBuyPrice.. '$ Ödedin'})
 
 	else
 
-        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'You dont have enough money!'})
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Yeterli miktarda paran yok!'})
 
 	end
 end)
